fc28ac74d1ec9f35569ebf0178b788f0fa188d46
rafi <rafinashrullah021@gmail.com>
2025-06-26 10:08:18 +0700

    fix

--- src/lib/type/pengumuman.ts
+++ src/lib/type/pengumuman.ts
@@ -1,8 +1,8 @@
 export type Pengumuman = {
   id: number;
   judul: string;
-  subjek: string;
-  isi: string;
+  subjek: string | null;
+  isi: string | null;
   tanggal: Date;
   rt_id: number;
   rukun_tetangga?: {
